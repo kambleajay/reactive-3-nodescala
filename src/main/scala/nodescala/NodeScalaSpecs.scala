6a3676521d8943268e25a161c0206943bc8a3b8a
Ajay <ajay.rivendell@gmail.com>
2013-11-25 23:32:30 +0530

    Added delay impl

--- src/main/scala/nodescala/NodeScalaSpecs.scala
+++ src/main/scala/nodescala/NodeScalaSpecs.scala
@@ -1,28 +1,38 @@
 package nodescala
 
-import org.scalacheck.Properties
+import org.scalacheck.{Gen, Properties}
 import org.scalacheck.Prop.{forAll, throws}
 import scala.concurrent.{Await, Future}
 import scala.language.postfixOps
 import scala.concurrent.duration._
-import java.util.concurrent.TimeoutException
+import java.util.concurrent.{TimeUnit, TimeoutException}
 
 class NodeScalaSpecs extends Properties("NodeScala") {
 
-  property("always returns with given value") = forAll { (aVal: AnyVal) =>
+  property("always") = forAll { (aVal: AnyVal) =>
     val actual = Future.always(aVal)
     Await.result(actual, 1 nanos) == aVal
   }
 
-  property("never does not create future") = forAll { (x: String) =>
+  property("never") = forAll { (x: String) =>
     val never = Future.never[x.type]
     throws(classOf[TimeoutException])(Await.result(never, 1 nanos))
   }
 
-  property("given a list of futures, returns a future of list containing all values") = forAll { (xs: List[String]) =>
+  property("all") = forAll { (xs: List[String]) =>
     val listOfFutures: List[Future[String]] = xs map (x => Future.always(x))
     val allInFuture: Future[List[String]] = Future.all(listOfFutures)
     Await.result(allInFuture, 1 millisecond) == xs
+  }
+
+  val durationGen = for {
+    length <- Gen.choose(10, 100)
+    time = TimeUnit.MILLISECONDS
+  } yield Duration(length, time)
+
+  property("delay") = forAll(durationGen) { (dur: Duration) =>
+    val f = Future.delay(dur)
+    throws(classOf[TimeoutException])(Await.result(f, dur - Duration(9, TimeUnit.MILLISECONDS)))
   }
 
 }
--- src/main/scala/nodescala/package.scala
+++ src/main/scala/nodescala/package.scala
@@ -53,7 +53,9 @@ package object nodescala {
 
     /** Returns a future with a unit value that is completed after time `t`.
      */
-    def delay(t: Duration): Future[Unit] = ???
+    def delay(t: Duration): Future[Unit] = async {
+      Thread.sleep(t.toMillis)
+    }
 
     /** Completes this future with user input.
      */
